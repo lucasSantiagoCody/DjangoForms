9b8a5d90f9610aabcffaa5ba6f528148b2332586
lucascody <lucassantiagocody@gmail.com>
2024-08-04 21:41:08 +0100

    Add comment in login_view_model_form view function

--- forms/views.py
+++ forms/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .forms import LoginModelForm
 
+
+# login view for LoginModelForm
 def login_view_model_form(request):
     context = {}
     context['form'] = LoginModelForm
