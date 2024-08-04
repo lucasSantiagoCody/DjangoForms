03b1aea41b771768721ee3105ea0776dd68e499c
lucascody <lucassantiagocody@gmail.com>
2024-08-04 11:00:41 +0100

    Create Profile model

--- forms/models.py
+++ forms/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female')
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=30)
+    phone = models.CharField(max_length=15)
+    birth_date = models.DateField()
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+
+    def __str__(self):
+        return self.user.username
+    
