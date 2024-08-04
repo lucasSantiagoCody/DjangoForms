77f560dffafc5d03276ffe9bc9d0e93b11b89c25
lucascody <lucassantiagocody@gmail.com>
2024-08-04 11:15:01 +0100

    Rename SignUpForm form to SignUpModelForm

--- forms/forms.py
+++ forms/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 
-class SignUpForm(forms.ModelForm):
+class SignUpModelForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
