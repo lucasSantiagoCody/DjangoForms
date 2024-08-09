ea22332b530f09f98d935bb06c15daa6801babff
lucascody <lucassantiagocody@gmail.com>
2024-08-09 19:47:37 +0100

    Remove url pattern for login_view_model_form and login_view_form from forms's urls.py file

--- forms/urls.py
+++ forms/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login-model-form/', views.login_view_model_form, name='login_model_form_url'),
-    path('login-form/', views.login_view_form, name='login_form_url'),
     path('signup-model-form/', views.signup_view_model_form, name='singup_model_form_url'),
     path('signup-form/', views.signup_view_form, name='signup_form_url'),
     path('signup-widget-form/', views.signup_view_widget_form, name='signup_widget_form_url'),
