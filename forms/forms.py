bd46dd90bd32d2f5c259bd3be34b1960611102f4
lucascody <lucassantiagocody@gmail.com>
2024-08-09 17:28:25 +0100

    Create SignUpForm

--- forms/forms.py
+++ forms/forms.py
@@ -1,5 +1,6 @@
-from django.forms import ModelForm
 from django.contrib.auth.models import User
+from django.forms import ModelForm
+from django import forms
 
 
 
@@ -7,9 +8,16 @@ class SignUpModelForm(ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+    
+
+class SignUpForm(forms.Form):
+    username = forms.CharField()
+    email = forms.EmailField()
+    password = forms.CharField()
 
 
 class LoginModelForm(ModelForm):
     class Meta:
         model = User
         fields = ['username', 'password']
+
