e8df65ff8a370d84aefd248c96cf138cf2fa6df7
lucascody <lucassantiagocody@gmail.com>
2024-08-09 17:32:00 +0100

    Create url pattern for login_view_form

--- forms/urls.py
+++ forms/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('login-model-form/', views.login_view_model_form, name='login_model_form_url'),
     path('signup-model-form/', views.signup_view_model_form, name='singup_model_form_url'),
     path('signup-form/', views.signup_view_form, name='signup_form_url'),
- 
+    path('login-form/', views.login_view_form, name='login_form_url'),
 
 ]
